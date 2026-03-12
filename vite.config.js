88e1b3f195ae0559cd61b7c58b5d4e9e25da4acb
Soniv Ahmad <sonivahmad@gmail.com>
2026-03-12 13:43:40 +0530

    Vite-config updated for splitting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
+          react: ['react', 'react-dom'],
+        }
+      }
+    }
+  }
 })
